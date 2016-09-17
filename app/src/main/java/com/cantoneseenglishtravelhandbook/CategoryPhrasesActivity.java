a56338377efc744cf5d7387e0265239aa0e0c0a8
dannyko22 <dannyko22@yahoo.com>
2016-09-17 11:18:07 -0400

    New commit

---
+++ app/src/main/java/com/cantoneseenglishtravelhandbook/CategoryPhrasesActivity.java
@@ -0,0 +1,91 @@
+package com.cantoneseenglishtravelhandbook;
+
+import android.app.FragmentManager;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.util.ArrayList;
+
+import static com.cantoneseenglishtravelhandbook.R.id.toolbar;
+
+public class CategoryPhrasesActivity extends AppCompatActivity {
+
+    ArrayList<TravelPhraseData> travelList;
+    ListView phrasesListView;
+    ArrayAdapter<String> phraseListViewAdapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_category_phrases);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("Phrases");
+
+        travelList = getIntent().getParcelableArrayListExtra("phrases");
+
+        setupPhrasesListView();
+
+    }
+
+
+    public void setupPhrasesListView() {
+        final Context context = this;
+        phrasesListView = (ListView) findViewById(R.id.phrasesListView);
+        populatePhrasesListView(travelList);
+        phrasesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+
+            }
+        });
+    }
+
+    public void populatePhrasesListView(ArrayList travelPhrasesList) {
+//        ArrayList<String> list = new ArrayList<String>();
+//        for (int i = 0; i < travelPhrasesList.size(); ++i) {
+//            TravelPhraseData tempTravelPhraseData = new TravelPhraseData();
+//            tempTravelPhraseData = (TravelPhraseData) travelPhrasesList.get(i);
+//            list.add(tempTravelPhraseData.getTravelPhrase());
+//        }
+        //phraseListViewAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, list);
+        //phrasesListView.setAdapter(phraseListViewAdapter);
+
+        PhrasesAdapterClass phrasesAdapter = new PhrasesAdapterClass(this, travelPhrasesList);
+        phrasesListView.setAdapter(phrasesAdapter);
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // handle arrow click here
+        if (item.getItemId() == android.R.id.home) {
+            finish(); // close this activity and return to preview activity (if there is any)
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/cantoneseenglishtravelhandbook/MainActivity.java
@@ -0,0 +1,213 @@
+package com.cantoneseenglishtravelhandbook;
+
+import android.content.Context;
+import android.content.Intent;
+import android.database.SQLException;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    DatabaseHelper myDbHelper;
+    ArrayList<TravelPhraseData> travelList;
+    ArrayList<TravelCategoryData> travelCategoryList;
+    RecyclerView categoryRecyclerView;
+    CategoryAdapterClass categoryAdapterClass;
+    LinearLayoutManager categoryLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final Context context = this;
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                //        .setAction("Action", null).show();
+
+                Intent intent = new Intent(context, activity_notepadrecycler.class);
+                startActivity(intent);
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        myDbHelper = new DatabaseHelper(this);
+
+
+
+        try {
+
+            myDbHelper.createDataBase();
+
+        } catch (IOException ioe) {
+
+            throw new Error("Unable to create database");
+
+        }
+
+        try {
+
+            myDbHelper.openDataBase();
+
+        }catch(SQLException sqle){
+
+            throw sqle;
+        }
+
+        travelCategoryList = new ArrayList<TravelCategoryData>();
+        travelCategoryList = myDbHelper.getAllCategoryData();
+
+        setupCategoryListView();
+    }
+
+    public void setupCategoryListView()
+    {
+        final Context context = this;
+
+        categoryRecyclerView = (RecyclerView) findViewById(R.id.recyclerViewCategory);
+        categoryRecyclerView.setHasFixedSize(true);
+        categoryLayoutManager = new LinearLayoutManager(this);
+        categoryRecyclerView.setLayoutManager(categoryLayoutManager);
+
+        final GestureDetector mGestureDetector =
+                new GestureDetector(this, new GestureDetector.SimpleOnGestureListener(){
+                    @Override
+                    public boolean onSingleTapUp(MotionEvent e) {
+                        return true;
+                    }
+                });
+
+        categoryRecyclerView.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
+
+
+            @Override
+            public boolean onInterceptTouchEvent(RecyclerView recyclerView, MotionEvent motionEvent) {
+
+
+                View child = recyclerView.findChildViewUnder(motionEvent.getX(), motionEvent.getY());
+
+                if (child != null && mGestureDetector.onTouchEvent(motionEvent)) {
+                    int position = recyclerView.getChildLayoutPosition(child);
+                    String category = travelCategoryList.get(position).getCategory();
+                    Intent intent = new Intent(context, CategoryPhrasesActivity.class);
+                    travelList = new ArrayList<TravelPhraseData>();
+                    travelList = myDbHelper.getTravelPhraseDatabyCategory(category);
+
+                    intent.putParcelableArrayListExtra("phrases", travelList);
+                    startActivity(intent);
+
+
+                }
+                return false;
+            }
+
+            @Override
+            public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+
+            }
+
+            @Override
+            public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+            }
+        });
+
+        populateCategoryListView(travelCategoryList);
+
+    }
+
+    public void populateCategoryListView(ArrayList travelCategoryList)
+    {
+        categoryAdapterClass = new CategoryAdapterClass(this, travelCategoryList);
+        categoryRecyclerView.setAdapter(categoryAdapterClass);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
