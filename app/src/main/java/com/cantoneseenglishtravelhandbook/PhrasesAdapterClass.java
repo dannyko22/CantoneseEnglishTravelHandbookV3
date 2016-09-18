7652fbf74bede41503b31b10865a597945acdce7
dannyko22 <dannyko22@yahoo.com>
2016-09-18 08:35:54 -0400

    added copy phrases to notebook.

--- app/src/main/java/com/cantoneseenglishtravelhandbook/PhrasesAdapterClass.java
+++ app/src/main/java/com/cantoneseenglishtravelhandbook/PhrasesAdapterClass.java
@@ -4,6 +4,7 @@ import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.res.ColorStateList;
+import android.database.SQLException;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.support.annotation.NonNull;
@@ -18,7 +19,9 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 
 import static android.content.Context.CLIPBOARD_SERVICE;
 
@@ -31,6 +34,7 @@ public class PhrasesAdapterClass extends ArrayAdapter {
     Context context;
     ArrayList<TravelPhraseData> travelPhraseData;
     LayoutInflater inflater;
+
 
     @NonNull
     @Override
@@ -56,10 +60,15 @@ public class PhrasesAdapterClass extends ArrayAdapter {
                 ListView parentListview = ((ListView) parentLinearLayout.getParent());
                 int position = parentListview.getPositionForView(view);
                 String phrase = travelPhraseData.get(position).getTravelPhrase() + "\n" + travelPhraseData.get(position).getHomePhrase() + "\n" + travelPhraseData.get(position).getPronounciation();
-                Toast.makeText(context, "Copied to clipboard" + "\n" + phrase, Toast.LENGTH_SHORT).show();
-                ClipboardManager clipboard = (ClipboardManager) context.getSystemService(CLIPBOARD_SERVICE);
-                ClipData clip = ClipData.newPlainText("label", phrase);
-                clipboard.setPrimaryClip(clip);
+                Toast.makeText(context, "Copied to Notepad" + "\n" + phrase, Toast.LENGTH_SHORT).show();
+//                ClipboardManager clipboard = (ClipboardManager) context.getSystemService(CLIPBOARD_SERVICE);
+//                ClipData clip = ClipData.newPlainText("label", phrase);
+//                clipboard.setPrimaryClip(clip);
+
+                NotepadDatabaseHelper notepadDBHelper;
+                notepadDBHelper = setupDatabaseHelper();
+                notepadDBHelper.insertNotepadData(travelPhraseData.get(position).getHomePhrase(), travelPhraseData.get(position).getTravelPhrase(), new Date());
+                notepadDBHelper.close();
             }
         });
         //return super.getView(position, convertView, parent);
@@ -72,5 +81,23 @@ public class PhrasesAdapterClass extends ArrayAdapter {
         this.context = _context;
         this.travelPhraseData = _travelPhraseData;
 
+    }
+
+    public NotepadDatabaseHelper setupDatabaseHelper()
+    {
+        NotepadDatabaseHelper notepadDBHelper = new NotepadDatabaseHelper(context);
+
+        try {
+            notepadDBHelper.createDataBase();
+        } catch (IOException ioe) {
+            throw new Error("Unable to create database");
+        }
+
+        try {
+            notepadDBHelper.openDataBase();
+        }catch(SQLException sqle){
+            throw sqle;
+        }
+        return notepadDBHelper;
     }
 }
